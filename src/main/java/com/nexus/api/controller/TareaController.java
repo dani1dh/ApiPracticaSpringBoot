b7bf8b6ec81364a6c7252b1f3e80c59e2efed594
Daniel Hernandez Abarca <danielhernd03@gmail.com>
2026-03-22 14:15:44 -0600

    Controller Tarea completo

--- src/main/java/com/nexus/api/controller/TareaController.java
+++ src/main/java/com/nexus/api/controller/TareaController.java
@@ -1,4 +1,47 @@
 package com.nexus.api.controller;
 
+import com.nexus.api.model.Tarea;
+import com.nexus.api.repository.TareaRepository;
+import com.nexus.api.service.TareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tareas")
 public class TareaController {
+
+    @Autowired
+    private TareaService tareaService;
+
+    @GetMapping
+    public ResponseEntity<List<Tarea>> listarTarea() {
+        return ResponseEntity.ok(tareaService.listarTareas());
+    }
+
+    @PostMapping
+    public ResponseEntity<Tarea> crearTarea(@RequestBody Tarea tarea) {
+        return ResponseEntity.created(URI.create("api/tarea/" + tarea.getId())).body(tareaService.crearTarea(tarea));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarea> traerTarea(@PathVariable Long id){
+        return ResponseEntity.ok(tareaService.traerTarea(id));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Tarea> modificarTarea(@PathVariable Long id, @RequestBody Tarea tarea){
+        return ResponseEntity.ok(tareaService.modificarTarea(id, tarea));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTarea(@PathVariable Long id){
+
+        tareaService.eliminarTarea(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
